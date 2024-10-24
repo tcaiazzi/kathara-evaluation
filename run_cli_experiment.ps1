4d5eefc76a49be104154df714c1c50ca6ccaba74
Tommaso Caiazzi <tommasocaiazzi@gmail.com>
2024-10-24 14:16:39 +0200

    Fix PowerShell CLI script

--- run_cli_experiment.ps1
+++ run_cli_experiment.ps1
@@ -22,7 +22,7 @@ foreach ($dir in Get-ChildItem -Path "$scenarios_path" -Directory) {
         echo "kathara lstart -d $labPath"
 
         Measure-Command { kathara lstart -d $labPath } | Select-Object -ExpandProperty TotalSeconds | Out-File -FilePath "$result_path\$os_name\time\$lab-log.txt" -Append
-        kathara linfo -d $dir | Select-String -Pattern '[0-9.]+\sMB' | ForEach-Object { $_.Matches.Value.Split(' ')[0] } | Out-File -FilePath "$result_path\$os_name\mem\lab-log-$i.txt"
+        kathara linfo -d .\network-scenarios\kathara-lab_bgp-ospf-rip\ | Select -skip 1 | Select-String -Pattern '[0-9]+[.][0-9]+ ' |  ForEach-Object { $_.Matches.Value.Split('|')[0] } | Out-File -FilePath "$result_path\$os_name\mem\$lab-log-$i.txt"
         kathara lclean -d $dir.FullName | Out-Null
     }
 
