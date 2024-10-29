fd3d25a5223f4c515b8531cba9a6e043d9185c1f
Tommaso Caiazzi <tommasocaiazzi@gmail.com>
2024-10-29 20:39:23 +0100

    Fix plot

--- plot.py
+++ plot.py
@@ -6,9 +6,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 platform_to_label = {
-    "win32": "windows",
-    "linux": "linux",
-    "darwin": "osx"
+    "win32": "Windows",
+    "linux": "Linux",
+    "darwin": "macOS"
 }
 
 # platform_to_color = {
@@ -22,6 +22,14 @@ platform_to_color = {
     "win32": "lightblue",
     "darwin": "lightgreen"
 }
+
+platform_to_error_color = {
+    "linux": "brown",
+    "win32": "blue",
+    "darwin": "green"
+}
+
+
 
 
 def extract_bars_values(path: str, field_name: str):
@@ -42,7 +50,8 @@ def extract_bars_values(path: str, field_name: str):
 
             results[platform][network_scenario_name] = {}
             results[platform][network_scenario_name]["y"] = statistics.mean(values)
-            results[platform][network_scenario_name]["dy"] = (statistics.stdev(values))
+            results[platform][network_scenario_name]["min_y"] = (min(values))
+            results[platform][network_scenario_name]["max_y"] = (max(values))
 
     return results
 
@@ -54,12 +63,21 @@ def plot_bars(parsed_results, ax):
 
     for idx, (platform, platform_result) in enumerate(parsed_results.items()):
         values_to_plot = list(map(lambda result: result["y"], platform_result.values()))
-        errors_to_plot = list(map(lambda result: result["dy"], platform_result.values()))
+        errors_to_plot = list(map(lambda result: (result['min_y'], result['max_y']), platform_result.values()))
         offset = width * multiplier
-        rects = ax.bar(x + offset, values_to_plot, width, yerr=errors_to_plot, label=platform_to_label[platform],
+        rects = ax.bar(x + offset, values_to_plot, width, label=platform_to_label[platform],
                        color=platform_to_color[platform], edgecolor='black', linewidth=1,
                        align='center', ecolor='black', capsize=10,
                        )
+
+        for i, y in enumerate(values_to_plot):
+            plt.errorbar(
+                x[i] + offset, y,
+                yerr=[[y - errors_to_plot[i][0]],
+                      [errors_to_plot[i][1] - y]],
+                color=platform_to_error_color[platform], elinewidth=1.5, capsize=1.5
+            )
+
         # ax.bar_label(rects, padding=3)
         multiplier += 1
 
@@ -72,12 +90,12 @@ def plot_memory_usage(path: str):
     fig, ax = plt.subplots(layout='constrained')
     ax.set_axisbelow(True)
     plt.grid(axis='y', linestyle='--', alpha=0.7)
-    ax.tick_params(axis='both', labelsize=14)
+    ax.tick_params(axis='both', labelsize=15)
 
     plot_bars(parsed_results, ax)
 
-    ax.set_ylabel('Memory Usage (MB)', fontsize=14)
-    ax.legend(fontsize=14)
+    ax.set_ylabel('Memory Usage (MB)', fontsize=15)
+    ax.legend(fontsize=15)
 
     plt.savefig(os.path.join(figures_path, "network_scenario_memory.pdf"), format="pdf", bbox_inches='tight')
 
@@ -88,11 +106,11 @@ def plot_startup_time(path: str):
     fig, ax = plt.subplots(layout='constrained')
     ax.set_axisbelow(True)
     plt.grid(axis='y', linestyle='--', alpha=0.7)
-    ax.tick_params(axis='both', labelsize=14)
+    ax.tick_params(axis='both', labelsize=15)
     plot_bars(parsed_results, ax)
 
-    ax.set_ylabel('Startup Time (sec)', fontsize=14)
-    ax.legend(fontsize=14)
+    ax.set_ylabel('Startup Time (sec)', fontsize=15)
+    ax.legend(fontsize=15)
 
     plt.savefig(os.path.join(figures_path, "network_scenario_startup_time.pdf"), format="pdf", bbox_inches='tight')
 
